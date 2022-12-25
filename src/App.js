3ae8594c34cdf9b833ec30b75ba5b896491f5758
Nurul Komor <58949982+nurul-komor@users.noreply.github.com>
2022-12-25 20:24:59 +0600

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Registration from './components/Registration'
+import Greetings from './components/Greetings'
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {
+        name: null,
+        email: null,
+        password: null
+      },
+      isRegistered: false
+    }
+  }
+  submitHandler = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    this.setState({ name, email, password, isRegistered: true }, () => { console.log(this.state) })
+  }
+  render() {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className="container mt-3  p-3">
+        {this.state.isRegistered ? <Greetings name={this.state.name} email={this.state.email} /> : <Registration submit={this.submitHandler} />}
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Registration.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Registration(props) {
+    return (
+        <div className='shadow-container card p-3'>
+            <h4>Registration form</h4>
+            <form onSubmit={props.submit}>
+                <div className="form-group">
+                    <label htmlFor="name">Name:</label>
+                    <input placeholder='type your name here...' type="text" className="form-control" name='name' id='name' />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input placeholder='type your email here...' type="email" className="form-control" name='email' id='email' />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input placeholder='password here...' type="password" className="form-control" name='password' id='password' />
+                </div>
+                <div className="form-group">
+                    <button type="submit" className='btn btn-primary'>Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
